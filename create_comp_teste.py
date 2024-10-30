ecc98b4f303f61b816171e5b84f5b7e59f657dfb
feramorimnando <fernandoelamorim@gmail.com>
2024-10-30 00:43:04 -0300

    Update create_comp_teste.py

--- create_comp_teste.py
+++ create_comp_teste.py
@@ -1,25 +1,29 @@
-# create_pet_table, populate_pet_table, get_all_pets, and get_birth_date are examples of tasks created by
-# instantiating the Postgres Operator
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.postgres_operator import PostgresOperator
+from datetime import datetime
 
-ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
+#ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
 DAG_ID = "create_table_teste"
 
 with DAG(
     dag_id=DAG_ID,
     start_date=datetime.datetime(2020, 2, 2),
-    schedule="@once",
+    schedule="@monthly",
     catchup=False,
-) as dag:
-    drop_table = SQLExecuteQueryOperator(
-        task_id="drop_table",
-        conn_id="datamart"
-        sql="""DROP TABLE IF EXISTS comp_test;""",
-    )
-   create_table = SQLExecuteQueryOperator(
-        task_id="create_table",
-        sql="""
+    ) as dag:
+    drop_table = PostgresOperator(
+        task_id = 'drop_table',
+        sql = """DROP TABLE IF EXISTS comp_teste;""",
+        postgres_conn_id = 'datamart',
+        autocommit = True)
+    create_table = PostgresOperator(
+        task_id = 'create_table',
+        sql = """
             CREATE TABLE comp_test AS select distinct ed.""Competência concessão"" from extract_datalake ed;
-            """,
-    )
+            """
+            ,
+        postgres_conn_id = 'datamart',
+        autocommit = True)
 
     drop_table >> create_table 
